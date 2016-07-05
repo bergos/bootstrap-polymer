de025a00fae1dc18f848454574cc4cc3a520c46d
bergi <bergi@axolotlfarm.org>
2016-07-05 21:40:03 +0200

    renamed main html to bootstrap-polymer.html and include polymer only in distribution

--- build.js
+++ build.js
@@ -4,4 +4,4 @@ if (!test('-d', 'dist')) {
   mkdir('dist')
 }
 
-exec('vulcanize --inline-scripts --inline-css --strip-comments index.html > dist/index.html')
+exec('vulcanize --inline-scripts --inline-css --strip-comments bootstrap-polymer-dist.html > dist/bootstrap-polymer-dist.html')
